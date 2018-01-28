5517132c53bc9ee659a54d51cbb284bbd9a85504
Clare <chblackburne@gmail.com>
2018-01-28 15:48:51 +0000

    Sets up fetch request and brings back array of currency quotes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ import Router from './Router';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currencyquotes: []
+    }
+    this.apiRequest = this.apiRequest.bind(this);
+  }
+  apiRequest(){
+    const url = "http://apilayer.net/api/live?access_key=0833e883a2f3f36c40bc7b7a5ebef033"
+    fetch(url)
+    .then(function(response){
+      if(response.ok){
+        console.log("Successful request");
+        return response.json();
+      }
+      throw new Error("Network response not OK")
+    })
+    .then(data => {
+      this.setState({currencyquotes: data});
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+  }
+
+  componentDidMount(){
+    this.apiRequest();
+  }
   render() {
+    console.log(this.state.currencyquotes);
     return (
       <div className="App">
         <Button raised color="primary">
