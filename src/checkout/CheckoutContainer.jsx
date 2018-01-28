a6706d8c768644575515be593cf3c4ea3cfd62de
Clare <chblackburne@gmail.com>
2018-01-28 16:54:54 +0000

    Links up selection menu with container component, passing through code of country selected

--- src/checkout/CheckoutContainer.jsx
+++ src/checkout/CheckoutContainer.jsx
@@ -4,13 +4,23 @@ import CurrencySelector from './CurrencySelector';
 class CheckoutContainer extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      chosenCurrency: ""
+    }
+    this.handleChosenCurrency = this.handleChosenCurrency.bind(this);
+  }
+
+  handleChosenCurrency(code){
+    //get chosen currency from selector
+    //compare against list of currencies
+  this.setState({chosenCurrency: code});
   }
   render(){
     console.log(this.props.data)
     return(
       <React.Fragment>
         <h1>checkout</h1>
-        <CurrencySelector data={this.props.data}/>
+        <CurrencySelector onSelect={this.handleChosenCurrency} data={this.props.data}/>
       </React.Fragment>
     )
   }
--- src/checkout/CurrencySelector.jsx
+++ src/checkout/CurrencySelector.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class CurrencySelector extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleSelection = this.handleSelection.bind(this);
+  }
+
 
   componentDidMount(){
     let countryOptions = this.props.data.countries;
@@ -9,18 +14,22 @@ class CurrencySelector extends React.Component {
         return <option>{country}</option>
     });
   }
+
+  handleSelection(event){
+    this.props.onSelect(event.target.value);
+    console.log(event.target.value);
+  }
   render(){
-    console.log(this.props.data.countries);
     let list = [];
     if(this.props.data.countries){
-    let countryOptions = this.props.data.countries;
-     list = Object.keys(countryOptions).map(code => {
-      return <option>{code}{countryOptions[code]}</option>
-    })
+      let countryOptions = this.props.data.countries;
+      list = Object.keys(countryOptions).map(code => {
+      return <option key={code} value={code}>{code}{countryOptions[code]}</option>
+    });
   }
 
     return(
-      <select>{list}</select>
+      <select onChange={this.handleSelection}>{list}</select>
     )
   }
 }
