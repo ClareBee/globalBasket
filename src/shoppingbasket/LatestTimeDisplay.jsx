c729b4e24a6a5512ee0d93989c668a876d41a080
Clare <chblackburne@gmail.com>
2018-01-28 21:32:19 +0000

    Installs moment.js and displays time rates last updated

--- src/shoppingbasket/LatestTimeDisplay.jsx
+++ src/shoppingbasket/LatestTimeDisplay.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 
 class LatestTimeDisplay extends React.Component{
   render(){
     console.log(this.props.data.currencyquotes.timestamp);
     let timeUnix = this.props.data.currencyquotes.timestamp;
-    let formattedTime = timeUnix;
+    let formattedTime = moment.unix(timeUnix).format('MMMM Do YYYY, h:mm:ss a');
     return(
       <React.Fragment>
-        <h2>Rates last updated:{formattedTime}</h2>
+        <h2>Rates last updated: {formattedTime}</h2>
       </React.Fragment>
     )
   }
