b320166e82dd0d09c542ed0ed6f879b6aa2a5a44
Clare <chblackburne@gmail.com>
2018-01-29 16:02:34 +0000

    Adds toggle function to switch between price in £ or $ symbols only

--- src/ProductData.js
+++ src/ProductData.js
@@ -6,5 +6,29 @@ export const ProductData = [
     name: "eggs",
     quantity: "dozen",
     price: 2.10
+  },
+  {
+    image: "https://www.trade-romania.ro/data/frontImages/b2b/buying_images/1379567937_Milk-3.jpg",
+    type: "dairy",
+    name: "milk",
+    quantity: "bottle",
+    price: 1.30,
+    currency: "USD"
+  },
+  {
+    image: "https://www.keytekshop.co.uk/wp-content/uploads/2015/05/Heinz-Beans-Decoy-Safe.jpg",
+    type: "storecupboard",
+    name: "beans",
+    quantity: "can",
+    price: 0.73,
+    currency: "USD"
+  },
+  {
+    image: "http://peashellers.net/wp-content/uploads/2015/04/Frozen-peas.jpg",
+    type: "frozen produce",
+    name: "peas",
+    quantity: "bag",
+    price: 0.95,
+    currency: "USD"
   }
 ]
--- src/shoppingbasket/ProductList.jsx
+++ src/shoppingbasket/ProductList.jsx
@@ -14,12 +14,14 @@ class ProductList extends React.Component{
   }
   render(){
     let products = this.props.products;
+    console.log(this.props);
     let display = products.map((product, index) => {
+
       return <GridListTile>
         <img src={product.image} />
         <GridListTileBar
           title={<span>{product.name}</span>}
-          subtitle={<span>{product.price}</span>}
+          subtitle={<span>{this.props.data.baseCurrency === "USDGBP" ? "£" : "$"}{product.price.toFixed(2)}</span>}
           actionIcon={
             <IconButton>
               <Icon color="primary" style={{ fontSize: 36 }}>
@@ -31,22 +33,21 @@ class ProductList extends React.Component{
       </GridListTile>
     })
 
-    console.log(this.props)
     return(
       <React.Fragment>
         <h1>Product list</h1>
-          <Grid container spacing={24}>
-            <Grid item xs={8}>
-              <GridList cols={2} cellHeight={250}>
+        <Grid container spacing={24}>
+          <Grid item xs={8}>
+            <GridList cols={2} cellHeight={250}>
 
-                {display}
+              {display}
 
-        </GridList>
-      </Grid>
-      <Grid item xs={4}>
-        <Basket />
-      </Grid>
-    </Grid>
+            </GridList>
+          </Grid>
+          <Grid item xs={4}>
+            <Basket />
+          </Grid>
+        </Grid>
       </React.Fragment>
     )
   }
